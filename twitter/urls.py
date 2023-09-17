6fc56b6ca88fc2ac7d95e76178b3447d147d3bb6
soheil <soheil@gmail.com>
2023-09-17 14:31:54 +0330

    7th-add follow and follow by

--- twitter/urls.py
+++ twitter/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home_page'),
+    path('profile_list/', views.ProfileList.as_view(), name='profile_list'),
+    path('profile_detail/<int:pk>', views.ProfileDetail.as_view(), name='profile_detail'),
+    path('follow/<int:pk>', views.Follow.as_view(), name='follow'),
+    path('unfollow/<int:pk>', views.Unfollow.as_view(), name='unfollow'),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import DetailView
 from django.views.generic.list import ListView
 from .models import Tweet
 from .forms import TweetForm
+from accounts.models import Profile
 
 
 class Home(View):
@@ -19,6 +21,7 @@ class Home(View):
     def post(self, request):
         if request.user.is_authenticated:
             form = TweetForm(request.POST, request.FILES)
+
             if form.is_valid():
                 tweet = form.save(commit=False)
                 tweet.user = request.user
@@ -33,3 +36,65 @@ class Home(View):
         else:
             user_tweets = Tweet.objects.all().order_by('-create_at')
             return render(request, 'twitter/home_page.html', {'user_tweets': user_tweets})
+
+
+class ProfileList(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            profiles = Profile.objects.exclude(user=request.user)
+            return render(request, 'twitter/profile_list.html', {'profiles': profiles})
+        else:
+            messages.success(request, 'You Must Be Logged In View This Page ...')
+            return redirect('home_page')
+
+
+class ProfileDetail(View):
+    def get(self, request, pk):
+        if request.user.is_authenticated:
+            profile = Profile.objects.get(user_id=pk)
+            tweets = Tweet.objects.filter(user_id=pk).order_by('-create_at')
+            return render(request, 'twitter/profile_detail.html', {'profile': profile, 'tweets': tweets})
+
+    def post(self, request, pk):
+        if request.user.is_authenticated:
+            profile = Profile.objects.get(user_id=pk)
+            tweets = Tweet.objects.filter(user_id=pk).order_by('-create_at')
+            current_user_profile = request.user.profile
+            action = request.POST['follow_name']
+            if action == 'unfollow':
+                current_user_profile.follows.remove(profile)
+                current_user_profile.save()
+            elif action == 'follow':
+                current_user_profile.follows.add(profile)
+                current_user_profile.save()
+
+            return render(request, 'twitter/profile_detail.html', {'profile': profile, 'tweets': tweets})
+        else:
+            messages.error(request, 'You Must Be Logged In View This Page ...', 'danger')
+            return redirect('home_page')
+
+
+class Follow(View):
+    def get(self, request, pk):
+        if request.user.is_authenticated:
+            profile = Profile.objects.get(user_id=pk)
+            request.user.profile.follows.add(profile)
+            request.user.profile.save()
+            messages.success(request, f'You Have Successfully Followed {profile.user.username}', 'success')
+            return redirect(request.META.get('HTTP_REFERER'))
+        else:
+            messages.error(request, 'That User Dose Not Exist...!', 'danger')
+            return redirect('home_page')
+
+
+class Unfollow(View):
+    def get(self, request, pk):
+        if request.user.is_authenticated:
+            profile = Profile.objects.get(user_id=pk)
+            request.user.profile.follows.remove(profile)
+            request.user.profile.save()
+            messages.success(request, f'You Have Successfully Unfollowed {profile.user.username}', 'success')
+            return redirect(request.META.get('HTTP_REFERER'))
+        else:
+            messages.error(request, 'That User Dose Not Exist...!', 'danger')
+            return redirect('home_page')
